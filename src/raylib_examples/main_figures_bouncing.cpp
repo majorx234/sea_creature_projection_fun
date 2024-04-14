c02f8ef3075aa6caf01104a893c26bafc0a4ecfe
MajorX234 <majorx234@googlemail.com>
2024-04-14 22:37:05 +0200

    Use function to calc movement.

--- src/raylib_examples/main_figures_bouncing.cpp
+++ src/raylib_examples/main_figures_bouncing.cpp
@@ -19,20 +19,20 @@ struct Figure{
   float y;
 };
 
-void move_simple(Figure figure, int screen_width, int screen_height) {
-  float nx = figure.x + figure.state.dx*figure.state.dt;
-  if (0.0 < nx - figure.texture.width/(2.0/figure.scale_ratio) && nx + figure.texture.width/(2.0/figure.scale_ratio) < screen_width) {
-    figure.x = nx;
+void move_simple(Figure *figure, int screen_width, int screen_height) {
+  float nx = figure->x + figure->state.dx*figure->state.dt;
+  if (0.0 < nx - figure->texture.width/(2.0/figure->scale_ratio) && nx + figure->texture.width/(2.0/figure->scale_ratio) < screen_width) {
+    figure->x = nx;
   } else {
-    figure.state.dx = -figure.state.dx;
+    figure->state.dx = -figure->state.dx;
   }
 
-  float ny = figure.y + figure.state.dy*figure.state.dt;
+  float ny = figure->y + figure->state.dy*figure->state.dt;
 
-  if (0 < ny - figure.texture.height/(2.0/figure.scale_ratio) && ny + figure.texture.height/(2.0/figure.scale_ratio) < screen_height) {
-    figure.y = ny;
+  if (0 < ny - figure->texture.height/(2.0/figure->scale_ratio) && ny + figure->texture.height/(2.0/figure->scale_ratio) < screen_height) {
+    figure->y = ny;
   } else {
-    figure.state.dy = -figure.state.dy;
+    figure->state.dy = -figure->state.dy;
   }
 }
 
@@ -40,60 +40,35 @@ int main(void){
   const int screen_width = 800;
   const int screen_height = 640;
   const int fps = 60;
-  InitWindow(800,600, "raylib test");
+  InitWindow(screen_width, screen_height, "raylib test");
 
   Texture2D sebastacean = LoadTexture("sebastacean.png");
-  MoveState sabastacean_state = {.dx = 200, .dy = 200, .dt =  1.0f/fps};
-  Figure sebastacean_fig = {.texture = sebastacean, .scale_ratio = 0.2, .state = sebastacean_state, .x = screen_width/2.0, .y =  screen_height/2.0};
+  MoveState sebastacean_state = {.dx = 200, .dy = 200, .dt =  1.0f/fps};
+  Figure sebastacean_fig = {.texture = sebastacean, .state = sebastacean_state, .scale_ratio = 0.2, .x = screen_width/2.0, .y =  screen_height/2.0};
   int rotation = 0;
-  float scale_ratio = 0.2;
-
-  int move_step = 0;
-  int move_steo_jump = 20;
 
   // Source rectangle (part of the texture to use for drawing)
   Rectangle source_rec = { 0.0f, 0.0f, (float)sebastacean.width, (float)sebastacean.height };
-  // Destination rectangle (screen rectangle where drawing part of texture)
 
   SetTargetFPS(fps);
 
-  float x = screen_width/2.0;
-  float y = screen_height/2.0;
-
-  float dx = 200;
-  float dy = 200;
-  float dt = 1.0f/fps;
-
-  int counter = 0;
   while(!WindowShouldClose()){
-    if (counter == move_steo_jump){
-      counter = 0;
-      move_step += 1;
-    }
-    else {
-      counter += 1;
-    }
-
-    float nx = x + dx*dt;
-    if (0.0 < nx - sebastacean.width/(2.0/scale_ratio) && nx + sebastacean.width/(2.0/scale_ratio) < screen_width) {
-      x = nx;
-    } else {
-      dx = -dx;
-    }
-
-    float ny = y + dy*dt;
-    if (0 < ny - sebastacean.height/(2.0/scale_ratio) && ny + sebastacean.height/(2.0/scale_ratio) < screen_height) {
-      y = ny;
-    } else {
-      dy = -dy;
-    }
-
-    Rectangle dest_rec = {screen_width, screen_height, sebastacean.width*scale_ratio, sebastacean.height*scale_ratio };
+    move_simple(&sebastacean_fig, screen_width, screen_height);
+    // Destination rectangle (screen rectangle where drawing part of texture)
+    Rectangle dest_rec = {
+      screen_width,
+      screen_height,
+      sebastacean_fig.texture.width*sebastacean_fig.scale_ratio,
+      sebastacean_fig.texture.height*sebastacean_fig.scale_ratio
+    };
 
     BeginDrawing();
     /* start drawing loop */
     ClearBackground(RAYWHITE);
-    Vector2 origin = { x + sebastacean.width * scale_ratio/2.0f, y + sebastacean.height * scale_ratio/2.0f };
+    Vector2 origin = {
+      sebastacean_fig.x + sebastacean.width * sebastacean_fig.scale_ratio/2.0f,
+      sebastacean_fig.y + sebastacean.height * sebastacean_fig.scale_ratio/2.0f
+    };
 
     DrawTexturePro(sebastacean, source_rec, dest_rec, origin, (float)rotation, WHITE);
     EndDrawing();
